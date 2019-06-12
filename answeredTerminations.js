a9860617ca42ed3f02d7bc9aa571717916754c83
Albert-Jan Nijburg <me@ajp.codes>
2019-06-12 12:27:55 +0100

    Revert "Add german message"

--- answeredTerminations.js
+++
@@ -1,4 +0,0 @@
-module.exports = [
-  new RegExp(/Terminate batch job \(Y\/N\)\?/g),
-  new RegExp(/Batchvorgang abbrechen \(J\/N\)\?/g),
-];
--- unansweredTerminations.js
+++
@@ -1,4 +0,0 @@
-module.exports = [
-  new RegExp(/Terminate batch job \(Y\/N\)\?[^\s]/g),
-  new RegExp(/Batchvorgang abbrechen \(J\/N\)\?[^\s]/g),
-];
